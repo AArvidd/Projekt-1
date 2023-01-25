deadca890d18ad0f211682ebab2a387069ab18a4
AArvidd <122522635+AArvidd@users.noreply.github.com>
2023-01-25 14:55:39 +0100

    Add files via upload
    25/1
    gjorde så att datorn kan veälja sina fiender och totalsluma fram fiender samt system för att kolla vem som har vunit

--- Projekt_1/src/projekt_1/Projekt_1.java
+++ Projekt_1/src/projekt_1/Projekt_1.java
@@ -24,6 +24,8 @@ public class Projekt_1 {
     static int[][] vit_trolkar = {{60, 30, 15, 10, 1, 5, 6}, 
                                   {0,  0,  0,  0,  0, 0, 1}};
     
+    static int[][] tom = {{0,0,0,0,0,0,0},{0,0,0,0,0,0,0}};
+    
     
     static int[][] sol1 = new int[7][2];
     static int[][] sol2 = new int[7][2];
@@ -44,6 +46,7 @@ public class Projekt_1 {
         int antal;
         
         do{
+            System.out.println();
             try{
                 System.out.print("Hur stor strid vill du ha?\n"
                                + "1: 1v1\n"
@@ -68,12 +71,140 @@ public class Projekt_1 {
         
         spelarVal(1,true);
         
+        sol2 = tom;
+        sol3 = tom;
+        sol4 = tom;
+        
+        if(antal>=2){
+            spelarVal(2,true);
+            if(antal>=3){
+                spelarVal(3,true);
+                if(antal==4){
+                    spelarVal(4,true);
+                }
+            }
+        }
+        
+        look.nextLine();
+        String val1;
+        
+        do{
+            System.out.print("\nVill du välga dinna motståndare y/n : ");
+            val1 = look.nextLine();
+            
+            if(!(val1.equals("y")||val1.equals("n"))){
+                System.out.print("ogiltigt svar");
+                continue;
+            }
+            
+            break;
+            
+        }while(true);
+        
+        if(val1.equals("Y")){
+            
+            spelarVal(1,false);
+            
+            fien2 = tom;
+            fien3 = tom;
+            fien4 = tom;
+            
+            if(antal>=2){
+                spelarVal(2,false);
+                if(antal>=3){
+                    spelarVal(3,false);
+                    if(antal==4){
+                        spelarVal(4,false);
+                    }
+                }
+            }
+          
+        }else{
+            //datorn slumpar motståndare
+            String val2;
+            do{
+                System.out.print("\nVill du ha förbestämda fiender y/n : ");
+                val2 = look.nextLine();
+                
+                if(!(val2.equals("y")||val2.equals("n"))){
+                    System.out.println("ogiltigt svar");
+                    continue;
+                }
+                
+                break;
+                
+            }while(true);
+            
+            if(val2.equals("y")){
+                
+                datorVal(1);
+                
+                fien2 = tom;
+                fien3 = tom;
+                fien4 = tom;
+                
+                if(antal>=2){
+                    datorVal(2);
+                    if(antal>=3){
+                        datorVal(3);
+                        if(antal==4){
+                            datorVal(4);
+                        }
+                    }
+                }
+                
+            }else{
+               fien1 = datorSlump(); 
+               
+               fien2 = tom;
+               fien3 = tom;
+               fien4 = tom;
+               
+               if(antal>=2){
+                    fien2 = datorSlump();
+                    if(antal>=3){
+                        fien3 = datorSlump();
+                        if(antal==4){
+                            fien3 = datorSlump();
+                        }
+                    }
+                }
+               
+            }
+        }
+        
+        for(int i = 0; i >=3 ; i++){
+            sol1[i][1] = sol1[i][0];
+            sol2[i][1] = sol2[i][0];
+            sol3[i][1] = sol3[i][0];
+            sol4[i][1] = sol4[i][0];
+            
+            fien1[i][1] = fien1[i][0];
+            fien2[i][1] = fien2[i][0];
+            fien3[i][1] = fien3[i][0];
+            fien4[i][1] = fien4[i][0];
+        }
+        
+        do{
+            
+        }while((sol1[0][1] > 0 && sol2[0][1] > 0 && sol3[0][1] > 0 && sol4[0][1] > 0) || (fien1[0][1] > 0 && fien2[0][1] > 0 && fien3[0][1] > 0 && fien4[0][1] > 0));
+        
+        if(sol1[0][1] > 0 && sol2[0][1] > 0 && sol3[0][1] > 0 && sol4[0][1] > 0){
+            System.out.println("DU VANN");
+        }else{
+            System.out.println("Du förlorade\nBättre lycka nästa gång");
+        }
     }
+    
+    
+    
+    
     
     static void spelarVal(int sol, boolean vän){
         Scanner look = new Scanner(System.in);
         int val;
         do{
+            System.out.println();
             if(vän){
                 System.out.print("Välj en soldat\n"
                                + "1: Fotsoldat\n"
@@ -242,5 +373,109 @@ public class Projekt_1 {
         }
     }
     
+    static void datorVal(int sol){
+        Random rand = new Random();
+        
+        int val = rand.nextInt(4)+1;
+        
+        switch(sol){
+            case 1:
+                switch(val){
+                   case 1:
+                        fien1 = fotsoldat;
+                        break;
+                    case 2:
+                        fien1 = riddare;
+                        break;
+                    case 3:
+                        fien1 = trolkar;
+                        break;
+                    case 4:
+                        fien1 = vit_trolkar;
+                        break;
+                }
+                break;
+            case 2:
+                switch(val){
+                    case 1:
+                        fien2 = fotsoldat;
+                        break;
+                    case 2:
+                        fien2 = riddare;
+                        break;
+                    case 3:
+                        fien2 = trolkar;
+                        break;
+                    case 4:
+                        fien2 = vit_trolkar;
+                        break;
+                }
+                break;
+            case 3:
+                switch(val){
+                    case 1:
+                        fien3 = fotsoldat;
+                        break;
+                    case 2:
+                        fien3 = riddare;
+                        break;
+                    case 3:
+                        fien3 = trolkar;
+                        break;
+                    case 4:
+                        fien4 = vit_trolkar;
+                        break;
+                }
+                break;
+            case 4:
+                switch(val){
+                    case 1:
+                        fien4 = fotsoldat;
+                        break;
+                    case 2:
+                        fien4 = riddare;
+                        break;
+                    case 3:
+                        fien4 = trolkar;
+                        break;
+                    case 4:
+                        fien4 = vit_trolkar;
+                        break;
+                }
+                break;
+        }
+        
+    }
     
+    static int[][] datorSlump(){
+        Random rand = new Random();
+        
+        int fien[][] = new int[7][2];
+        
+        fien[0][0] = (rand.nextInt(20)+1)*5+50;
+        
+        for(int i = 1; i<4; i++){
+            fien[i][0] = rand.nextInt(5)*5+10;
+        }
+        
+        fien[4][0] = 1;
+        
+        fien[5][0] = rand.nextInt(6)+1;
+        
+        if(fien[5][0] == 0){
+            fien[5][0] = 0;
+            return fien;
+        }
+        
+        do{
+            fien[6][0] = rand.nextInt(6)+1;
+            if(fien[6][0] == 1){
+                fien[6][0] = 0;
+            }
+        }while(fien[5][0] == fien[6][0]);
+        
+        fien[6][1] = 1;
+        
+        return fien;
+    }
 }
